1b8b1909386719335796029283fc1bd0f592e762
Chicagolil <he202305@students.ephec.be>
2024-10-16 12:36:06 +0200

    Soucis Variables d'environements - réglé
    la connection à la base de donnée était bloqué parce que j'arrivais pas à charger les variables d'environment dans le bon fichier mais c'est tout bon maintenant

--- back/server.js
+++ back/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import recordRoutes from "./routes/record.js"; // Ajout de la route
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/records", recordRoutes);
 
 // start the Express server
 app.listen(PORT, () => {
