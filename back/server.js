ef1a7da0f09ad66d68c4eec1edc844a8e163a3a2
Chicagolil <he202305@students.ephec.be>
2024-10-23 12:02:07 +0200

    Connection au broker MQTT + Affichage dans le front
    On se connecte au broker mqtt en s'abonnant au topic smartpaws/niveau pour récuperer les données du capteur qui arrivent depuis le raspberry
    une fois qu'on les a on les stocke temporairement dans node et on expose une api pour venir les récupérer depuis le front
    dans l'api
    Et ducoup dans le front je viens interroger l'api toutes les 5 secondes pour récuperer les données

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import recordRoutes from "./routes/record.js"; // Ajout de la route
+import mqtt from "mqtt";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -8,6 +9,31 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/records", recordRoutes);
+
+let currentLevels = { croquettes: 0 }; // Stockage temporaire des données
+
+// Configuration du client MQTT
+const mqttClient = mqtt.connect("mqtt://localhost:1883"); // Adresse du MQTT local
+
+mqttClient.on("connect", () => {
+  console.log("Connecté au broker MQTT");
+  mqttClient.subscribe("smartpaws/niveau", (err) => {
+    if (err) {
+      console.error("Erreur d'abonnement au topic MQTT", err);
+    }
+  });
+});
+
+// Réception des messages publiés sur le topic
+mqttClient.on("message", (topic, message) => {
+  currentLevels = JSON.parse(message.toString());
+  console.log("Nouveaux niveaux reçus :", currentLevels);
+});
+
+// API pour renvoyer les niveaux de croquette actuels
+app.get("/api/niveau", (req, res) => {
+  res.json(currentLevels);
+});
 
 // start the Express server
 app.listen(PORT, () => {
