f34fe0979d6057d539b265cf16fe07b7d41f1f7c
Julien Pollart <jpollart@gmail.com>
2024-10-24 09:23:28 +0200

    Fixing button issues for settings

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -1,49 +1,65 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import SettingsIcon from "../Assets/Images/settings.svg";
 import { Header, Level, NotificationsContainer } from "../Containers";
-import React, { useEffect, useState } from "react";
 
 const HomePage: React.FC = () => {
-  const [niveaux, setNiveaux] = useState({ croquettes: 0 });
+	const [niveaux, setNiveaux] = useState({ croquettes: 0 });
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchLevels = async () => {
-      try {
-        const response = await fetch("http://localhost:5050/api/niveau"); // Appel API backend
-        if (!response.ok) {
-          throw new Error("Erreur lors de la récupération des données");
-        }
-        const data = await response.json();
-        setNiveaux(data); // Met à jour l'état avec les données récupérées
-      } catch (error) {
-        console.error("Erreur :", error);
-      }
-    };
+	useEffect(() => {
+		const fetchLevels = async () => {
+			try {
+				const response = await fetch(
+					"http://localhost:5050/api/niveau"
+				); // Appel API backend
+				if (!response.ok) {
+					throw new Error(
+						"Erreur lors de la récupération des données"
+					);
+				}
+				const data = await response.json();
+				setNiveaux(data); // Met à jour l'état avec les données récupérées
+			} catch (error) {
+				console.error("Erreur :", error);
+			}
+		};
 
-    fetchLevels(); // Charger les données au montage
-    const interval = setInterval(fetchLevels, 5000); // Actualiser toutes les 5 secondes
-    return () => clearInterval(interval); // Nettoyer l'intervalle à la destruction du composant
-  }, []);
+		fetchLevels(); // Charger les données au montage
+		const interval = setInterval(fetchLevels, 5000); // Actualiser toutes les 5 secondes
+		return () => clearInterval(interval); // Nettoyer l'intervalle à la destruction du composant
+	}, []);
 
-  return (
-    <Box className="h-auto bg-bg flex flex-col gap-9">
-      <Header />
-      <Box>
-        <Box className="pb-12 p-3">
-          <img src={SettingsIcon} className="size-12" alt="Icon paramètre" />
+	return (
+		<Box className="h-auto bg-bg flex flex-col gap-9">
+			<Header />
+			<Box>
+				<Box className="pb-12 p-3">
+					<Button
+						className="cursor-pointer"
+						onClick={() => navigate("/settings")}>
+						<img
+							src={SettingsIcon}
+							alt="Icon paramètre"
+							className="size-12"
+						/>
+					</Button>
 
-          <Level label="Eau" progress={60} />
-          {/* Pour changer la valeur de la bar il faut mettre le nombre en pourcentage */}
-          <Level label="Nourriture" progress={niveaux.croquettes} />
-          {/* Pour changer la valeur de la bar il faut mettre le nombre en pourcentage */}
-        </Box>
-        <Box className="flex w-full p-3">
-          <Box className="w-1/2">A voir quel container mettre ici</Box>
-          <NotificationsContainer />
-        </Box>
-      </Box>
-    </Box>
-  );
+					<Level label="Eau" progress={60} />
+					{/* Pour changer la valeur de la bar il faut mettre le nombre en pourcentage */}
+					<Level label="Nourriture" progress={niveaux.croquettes} />
+					{/* Pour changer la valeur de la bar il faut mettre le nombre en pourcentage */}
+				</Box>
+				<Box className="flex w-full p-3">
+					<Box className="w-1/2">
+						A voir quel container mettre ici
+					</Box>
+					<NotificationsContainer />
+				</Box>
+			</Box>
+		</Box>
+	);
 };
 
 export default HomePage;
