4f0b4d8b6536bb4cbe3d93e6e5612660d040a8cf
Chicagolil <he202305@students.ephec.be>
2024-10-29 13:36:53 +0100

    les barres de progression passe en rouge en dessous de 20%

--- front/src/Components/ProgressBar.tsx
+++ front/src/Components/ProgressBar.tsx
@@ -1,19 +1,21 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import { Box } from "@mui/material";
+import React from "react";
 
 interface ProgressBarProps {
-	progress: number;
+  progress: number;
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
-	return (
-		<Box className='relative w-full h-2'>
-			<Box className='absolute w-full h-2 rounded-full bg-text'></Box>
-			<Box
-				className='absolute h-2 rounded-full bg-main'
-				style={{ width: `${progress}%` }}></Box>
-		</Box>
-	);
+  const barColor = progress < 20 ? "bg-red-500" : "bg-main"; // Changer la couleur en rouge si progress est < 20
+  return (
+    <Box className="relative w-full h-2">
+      <Box className="absolute w-full h-2 rounded-full bg-text"></Box>
+      <Box
+        className={`absolute h-2 rounded-full ${barColor}`} // Applique la couleur basée sur le stock
+        style={{ width: `${progress}%` }}
+      ></Box>
+    </Box>
+  );
 };
 
 export default ProgressBar;
