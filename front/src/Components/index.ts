eb1304bf54249dd3effa7c8227cd745b4ef050c7
Julien Pollart <jpollart@gmail.com>
2024-12-11 11:05:03 +0100

    Suite us perso

--- front/src/Components/index.ts
+++ front/src/Components/index.ts
@@ -1,2 +1,3 @@
-export { default as NotificationItems } from "./NotificationItems";
-export { default as ProgressBar } from "./ProgressBar";
+export { default as ChartComponent } from "./chartComponent";
+export { default as NotificationItems } from "./notificationItems";
+export { default as ProgressBar } from "./progressBar";
