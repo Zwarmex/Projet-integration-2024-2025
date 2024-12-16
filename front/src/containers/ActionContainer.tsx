659add856ba1173245886d322b6d98dcf1aad970
Julien Pollart <jpollart@gmail.com>
2024-12-16 16:54:54 +0100

    Ajout récupération dynamique page settings + icone graphique + ajout mui material icon (icones)

--- front/src/containers/ActionContainer.tsx
+++ front/src/containers/ActionContainer.tsx
@@ -3,67 +3,68 @@ import React from "react";
 import { useUrl } from "../Context/UrlContext";
 
 const ActionContainer: React.FC = () => {
-  const { url } = useUrl();
+	const { url } = useUrl();
 
-  const distribuerCroquettes = async () => {
-    try {
-      const response = await fetch(`${url}api/distribuer_croquettes`);
-      const data = await response.json();
-      console.log(data.message); // Affiche la réponse dans la console
-    } catch (error) {
-      console.error(
-        "Erreur lors de l'appel à l'API distribuer_croquettes :",
-        error
-      );
-    }
-  };
+	const distribuerCroquettes = async () => {
+		try {
+			const response = await fetch(`${url}api/distribuer_croquettes`);
+			const data = await response.json();
+			console.log(data.message); // Affiche la réponse dans la console
+		} catch (error) {
+			console.error(
+				"Erreur lors de l'appel à l'API distribuer_croquettes :",
+				error
+			);
+		}
+	};
 
-  const distribuerEau = async () => {
-    try {
-      const response = await fetch(`${url}api/distribuer_eau`);
-      const data = await response.json();
-      console.log(data.message); // Affiche la réponse dans la console
-    } catch (error) {
-      console.error("Erreur lors de l'appel à l'API distribuer_eau :", error);
-    }
-  };
+	const distribuerEau = async () => {
+		try {
+			const response = await fetch(`${url}api/distribuer_eau`);
+			const data = await response.json();
+			console.log(data.message); // Affiche la réponse dans la console
+		} catch (error) {
+			console.error(
+				"Erreur lors de l'appel à l'API distribuer_eau :",
+				error
+			);
+		}
+	};
 
-  const distribuerFriandises = async () => {
-    try {
-      const response = await fetch(`${url}api/distribuer_friandises`);
-      const data = await response.json();
-      console.log(data.message); // Affiche la réponse dans la console
-    } catch (error) {
-      console.error(
-        "Erreur lors de l'appel à l'API distribuer_friandises :",
-        error
-      );
-    }
-  };
-  return (
-    <Box className="border-solid border-2 border-black sm:gap-0 gap-3 rounded-lg sm:w-1/2 p-3 flex flex-col justify-around">
-      <Button
-        onClick={distribuerCroquettes}
-        variant="contained"
-        sx={{
-          backgroundColor: "var(--main)", // Utiliser la couleur de Tailwind
-          color: "white",
-        }}
-      >
-        Distribuer Nourriture
-      </Button>
-      <Button onClick={distribuerEau} variant="contained">
-      Distribuer Eau
-      </Button>
-      <Button
-        onClick={distribuerFriandises}
-        variant="contained"
-        style={{ backgroundColor: "red", color: "white" }}
-      >
-        Distribuer Friandise
-      </Button>
-    </Box>
-  );
+	const distribuerFriandises = async () => {
+		try {
+			const response = await fetch(`${url}api/distribuer_friandises`);
+			const data = await response.json();
+			console.log(data.message); // Affiche la réponse dans la console
+		} catch (error) {
+			console.error(
+				"Erreur lors de l'appel à l'API distribuer_friandises :",
+				error
+			);
+		}
+	};
+	return (
+		<Box className="border-solid border-2 border-black sm:gap-0 gap-3 rounded-lg sm:w-1/2 p-3 flex flex-col justify-around">
+			<Button
+				onClick={distribuerCroquettes}
+				variant="contained"
+				sx={{
+					backgroundColor: "var(--main)", // Utiliser la couleur de Tailwind
+					color: "white",
+				}}>
+				Remplir Nourriture
+			</Button>
+			<Button onClick={distribuerEau} variant="contained">
+				Remplir Eau
+			</Button>
+			<Button
+				onClick={distribuerFriandises}
+				variant="contained"
+				style={{ backgroundColor: "red", color: "white" }}>
+				Donner Friandise
+			</Button>
+		</Box>
+	);
 };
 
 export default ActionContainer;
